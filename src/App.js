a5bb87b30f233a4281ed596a7657144e7895f397
Devendra <devendranathreddy54@gmail.com>
2024-01-08 20:50:42 +0530

    comp 3

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
 import Users from "./users/pages/Users";
+import MainNavigation from "./common/components/Navigation/MainNavigation";
+import Userplaces from "./places/pages/Userplaces";
+import Newplaces from "./places/pages/Newplaces";
 
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route path='/'>
-          <Users/>
-        </Route>
-        <Redirect to='/' />
-      </Switch>
+      <MainNavigation />
+      <main>
+        <Switch>
+          <Route path="/" exact>
+            <Users />
+          </Route>
+          <Route path="/:userid/places" exact>
+            <Userplaces />
+          </Route>
+          <Route path="/places/new" exact>
+            <Newplaces />
+          </Route>
+        </Switch>
+      </main>
     </Router>
   );
 }
