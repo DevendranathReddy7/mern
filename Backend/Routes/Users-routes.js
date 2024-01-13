e6cb580698f792f58f4fa538c42e6b1892afc49d
Devendra <devendranathreddy54@gmail.com>
2024-01-13 12:47:46 +0530

    adding routes to application

--- Backend/Routes/Users-routes.js
+++ Backend/Routes/Users-routes.js
@@ -1,24 +1,24 @@
 const express = require("express");
+const userControllers = require("../Controllers/Users-controller");
+const { check } = require("express-validator");
 const router = express.Router();
 
-const USERS = [
-  {
-    id: "u1",
-    name: "Dev",
-    image:
-      "https://images.pexels.com/photos/839011/pexels-photo-839011.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-    places: 3,
-  },
-  {
-    id: "u2",
-    name: "Devendra Reddy",
-    image:
-      "https://images.pexels.com/photos/839011/pexels-photo-839011.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-    places: 2,
-  },
-];
-router.get("/users/:uid", (req, res, next) => {
-  res.json({ user: "I am user" });
-});
+router.get("/", userControllers.getUsers);
+
+router.post(
+  "/login",
+  [check("email").not().isEmail(), check("password").not().isEmpty()],
+  userControllers.loginUser
+);
+
+router.post(
+  "/signup",
+  [
+    check("name").not().isEmpty(),
+    check("email").normalizeEmail().isEmail(),
+    check("password").not().isEmpty(),
+  ],
+  userControllers.signUpUser
+);
 
 module.exports = router;
--- Backend/app.js
+++ Backend/app.js
@@ -4,8 +4,23 @@ const app = express();
 
 const placesRoutes = require("./Routes/Places-routes.js");
 const userRoutes = require("./Routes/Users-routes.js");
+const HttpError = require("./Models/http-error.js");
 
+app.use(bodyParser.json());
 app.use("/api/places", placesRoutes);
-app.use("/api/places", userRoutes);
+
+app.use("/api/users", userRoutes);
+
+app.use((req, res, next) => {
+  throw new HttpError("Could not find the route", 404);
+});
+
+app.use((error, req, res, next) => {
+  if (res.headerSent) {
+    return next(error);
+  }
+  res.status(error.code || 500);
+  res.json({ message: error.message || "An unknown error occurred" });
+});
 
 app.listen(5000);
