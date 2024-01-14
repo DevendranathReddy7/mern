582520d48bca694139672b8a53711aec662361f5
Devendra <devendranathreddy54@gmail.com>
2024-01-14 09:52:41 +0530

    started with connecting backend with frontend

--- Backend/app.js
+++ Backend/app.js
@@ -8,6 +8,14 @@ const userRoutes = require("./Routes/Users-routes.js");
 const HttpError = require("./Models/http-error.js");
 
 app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers", "*"); //Origin,X-Requested-With,Content-Type,Accept,Authorization
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PATCH,DELETE");
+
+  next();
+});
 app.use("/api/places", placesRoutes);
 
 app.use("/api/users", userRoutes);
